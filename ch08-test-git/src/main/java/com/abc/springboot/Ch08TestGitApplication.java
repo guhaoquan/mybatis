38fc399f71c8b2baae78fdfe0c133d72bb9627fe
guhaoquan <1162759071@qq.com>
2021-05-27 16:09:59 +0800

    测试增加一个项目，模拟分布式

---
+++ ch08-test-git/src/main/java/com/abc/springboot/Ch08TestGitApplication.java
@@ -0,0 +1,13 @@
+package com.abc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch08TestGitApplication.class, args);
+    }
+
+}
